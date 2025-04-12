2cbde8c0cebf823238d251dfba656f0ccf86b147
Renggeng Zheng <rzhengcontacts@gmail.com>
2025-04-12 16:41:30 -0400

    provisional pure pursuit draft

--- path_planning/trajectory_follower.py
+++ path_planning/trajectory_follower.py
@@ -1,7 +1,10 @@
 import rclpy
 from ackermann_msgs.msg import AckermannDriveStamped
-from geometry_msgs.msg import PoseArray
+from geometry_msgs.msg import Odometry, Pose, PoseArray
 from rclpy.node import Node
+
+import numpy as np
+import numpy.typing as npt
 
 from .utils import LineTrajectory
 
@@ -29,26 +32,94 @@ class PurePursuit(Node):
         self.wheelbase_length: float = self.get_parameter('wheelbase_length').get_parameter_value().double_value
 
         # Subscribers to the planned path and publishers for the drive command.
-        self.trajectory = LineTrajectory("/followed_trajectory")
+        self.trajectory: LineTrajectory = LineTrajectory("/followed_trajectory")
 
         self.traj_sub = self.create_subscription(
             PoseArray, "/trajectory/current",
             self.trajectory_callback, 1
         )
+        self.odom_sub = self.create_subscription(
+            Odometry, self.odom_topic,
+            self.pose_callback, 1
+        )
         self.drive_pub = self.create_publisher(
             AckermannDriveStamped, self.drive_topic, 1
         )
 
-    def pose_callback(self, odometry_msg):
-        raise NotImplementedError
+    def pose_callback(self, odometry_msg: Odometry):
+        """
+        Takes the current position of the robot, finds the nearest point on the
+        path, sets that as the goal point, and navigates towards it.
+        """
+        # Gets vectorized Pose of the robot.
+        pose: Pose = odometry_msg.pose.pose
+        position: npt.NDArray = np.array([pose.position.x, pose.position.y])
+        heading: np.float64 = 2 * np.arccos2(pose.orientation.w)
+
+        # Finds the path point closest to the robot.
+        if not self.initialized_traj:
+            return
+        
+        # Calculates the distance to all points in the trajectory, vectorized.
+        trajectory_points: npt.NDArray[np.float64] = np.array(
+            self.trajectory.points
+        )
+        distances: npt.NDArray[np.float64] = np.linalg.norm(
+            trajectory_points - position, axis=1
+        )
+
+        # Finds the index of the closest point.
+        closest_index: int = np.argmin(distances)
+        closest_point: npt.NDArray[np.float64] = trajectory_points[closest_index]
+
+        # Finds the lookahead/goal point.
+        for i in range(closest_index, len(trajectory_points)):
+            # If the distance is equal, set it as the goal point.
+            if distances[i] == self.lookahead:
+                goal_point: npt.NDArray[np.float64] = trajectory_points[i]
+                break
+            # If not, interpolate between the two points.
+            elif distances[i] > self.lookahead:
+                goal_point: npt.NDArray[np.float64] = (
+                    trajectory_points[i - 1] +
+                    (self.lookahead - distances[i - 1]) /
+                    (distances[i] - distances[i - 1]) *
+                    (trajectory_points[i] - trajectory_points[i - 1])
+                )
+                break
+        
+        # Transform the goal point to the robot's frame.
+        goal_point = np.array([
+            goal_point[0] - position[0],
+            goal_point[1] - position[1]
+        ])
+        # Rotate the goal point by the robot's heading.
+        goal_point = np.array([
+            goal_point[0] * np.cos(heading) + goal_point[1] * np.sin(heading),
+            -goal_point[0] * np.sin(heading) + goal_point[1] * np.cos(heading)
+        ])
+
+        # Calculate the curvature 
+        gamma: float = 2 * goal_point[0] / (self.lookahead ** 2)
+        # Calculate the steering angle.
+        steering_angle: float = np.arctan(gamma * self.wheelbase_length)
+        
+        # Create the drive command.
+        drive_cmd: AckermannDriveStamped = AckermannDriveStamped()
+        drive_cmd.drive.speed = self.speed
+        drive_cmd.drive.steering_angle = steering_angle
+        drive_cmd.drive.steering_angle_velocity = 0.0
+        # Publish the drive command.
+        self.drive_pub.publish(drive_cmd)
 
     def trajectory_callback(self, msg):
         self.get_logger().info(f"Receiving new trajectory {len(msg.poses)} points")
 
         self.trajectory.clear()
+        # Converts from poses to the utility trajectory class.
         self.trajectory.fromPoseArray(msg)
         self.trajectory.publish_viz(duration=0.0)
-
+        # flag to check that we have a trajectory.
         self.initialized_traj = True
 
 
